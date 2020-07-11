71e7ac5d74d31c5c21d36ec8559f4ec701d5b880
Tom Moor <tom.moor@gmail.com>
2020-07-11 10:11:55 -0700

    staging -> www

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ rm -rf node_modules/.cache
 next build
 next export
 touch out/.nojekyll
-echo staging.getoutline.com >> out/CNAME
+echo www.getoutline.com >> out/CNAME
 git add out/
 git commit -m "Deploy to gh-pages"
 git subtree push --prefix out origin gh-pages
