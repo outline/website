36da590aa8b26e0f47a09d8274bf4531bc430f3f
Tom Moor <tom.moor@gmail.com>
2020-07-25 16:12:08 -0700

    style tweaks

--- components/Markdown.tsx
+++ components/Markdown.tsx
@@ -26,10 +26,10 @@ export default function Markdown(props) {
           }
 
           .md :global(img) {
+            display: block;
             max-width: 100%;
             zoom: 50%;
-            box-shadow: 10px 10px 0 rgba(0, 0, 0, 0.1),
-              -1px -1px 0 rgba(0, 0, 0, 0.1);
+            box-shadow:  0 0 0 1px rgba(0, 0, 0, 0.2);
             border-radius: 8px;
             margin: 2em auto;
           }
