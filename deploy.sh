dd4cc7ad1a1b608091021efda59068761ef27748
Tom Moor <tom.moor@gmail.com>
2020-07-25 19:14:41 -0700

    chore: More reliable deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ touch out/.nojekyll
 echo www.getoutline.com >> out/CNAME
 git add out/
 git commit -m "Deploy to gh-pages"
-git subtree push --prefix out origin gh-pages
+git push origin `git subtree split --prefix out develop`:gh-pages --force
