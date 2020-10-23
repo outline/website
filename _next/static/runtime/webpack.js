560b5416a60c511e5b4ea45e44402f7153c36651
Tom Moor <tom.moor@gmail.com>
2020-10-22 20:57:15 -0700

    Deploy to gh-pages

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3eb571427129d09dd0d7";
+/******/ 	var hotCurrentHash = "77a5df680ce71ce88598";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
