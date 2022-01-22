c7a39c801e5ae8570e0e52c219e1d5315460ba13
Tom Moor <tom.moor@gmail.com>
2022-01-22 08:46:24 -0800

    Add headers

---
+++ next.config.js
@@ -0,0 +1,24 @@
+const securityHeaders = [
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+];
+
+module.exports = {
+  poweredByHeader: false,
+
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
+  },
+};
