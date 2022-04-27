abece4342ec880afee8f7f30ea22faa8e23a1183
Tom Moor <tom.moor@gmail.com>
2022-04-26 21:28:03 -0700

    feat: Remove Typeform contact form

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,27 +1,11 @@
 import Layout from "components/Layout";
 import * as React from "react";
 import { colors } from "theme";
+import { darken } from "polished";
+import { useRouter } from "next/router";
 
-export default function Privacy() {
-  const callback = React.useCallback(() => {
-    var qs,
-      js,
-      q,
-      s,
-      d = document,
-      gi = d.getElementById,
-      ce = d.createElement,
-      gt = d.getElementsByTagName,
-      id = "typef_orm",
-      b = "https://embed.typeform.com/";
-    if (!gi.call(d, id)) {
-      js = ce.call(d, "script");
-      js.id = id;
-      js.src = b + "embed.js";
-      q = gt.call(d, "script")[0];
-      q.parentNode.insertBefore(js, q);
-    }
-  }, []);
+export default function ContactUs() {
+  const { query } = useRouter();
 
   return (
     <Layout
@@ -30,41 +14,134 @@ export default function Privacy() {
       hero="How to get in touch with the team"
     >
       <div className="container">
-        <p>
-          You can email us directly at{" "}
-          <a href="mailto:hello@getoutline.com">hello@getoutline.com</a> from
-          your favorite email client, or reach out on twitter{" "}
-          <a href="https://twitter.com/getoutline" target="_blank">
-            @getoutline
-          </a>
-          .
-        </p>
-        <p>
-          We aim to reply to all enquiries within 24 hours, but we often reply
-          much faster than that during PST waking hours. To make things easy we
-          also have a direct contact form below that you can fill out in just a
-          few clicks.
-        </p>
-
-        <div
-          ref={callback}
-          className="typeform-widget"
-          data-url="https://form.typeform.com/to/tQ5Uoqi8"
-          data-transparency="50"
-          data-hide-headers="true"
-          data-hide-footer="true"
-        ></div>
+        <div className="pure-g">
+          <div className="pure-u-1 pure-u-md-2-5">
+            <div className="form-wrapper">
+              {query.success ? (
+                <p>
+                  Thank you for your message. We will get back to you as soon as
+                  possible.
+                </p>
+              ) : (
+                <form
+                  action="http://localhost:3000/api/contactMessage.create"
+                  method="POST"
+                >
+                  <label>
+                    <div>Your name</div>
+                    <input type="text" name="name" required />
+                  </label>
+                  <label>
+                    <div>Email address</div>
+                    <input type="email" name="replyTo" required />
+                  </label>
+                  <label>
+                    <div>Contact reason</div>
+                    <select name="subject" required>
+                      <option disabled value="">
+                        Contact reason…
+                      </option>
+                      <option value="I'm having problems and need help">
+                        I'm having problems and need help
+                      </option>
+                      <option value="Feedback and product ideas">
+                        Feedback and product ideas
+                      </option>
+                      <option value="Request an enterprise trial">
+                        Request an enterprise trial
+                      </option>
+                      <option value="Something else">Something else</option>
+                    </select>
+                  </label>
+                  <label>
+                    <div>Deployment</div>
+                    <select name="deployment" required>
+                      <option value="cloud">Cloud (getoutline.com)</option>
+                      <option value="enterprise">Enterprise</option>
+                      <option value="self-hosted">Self-Hosted</option>
+                    </select>
+                  </label>
+                  <label>
+                    <div>Your message</div>
+                    <textarea
+                      name="text"
+                      placeholder="What can we help with?"
+                      required
+                    />
+                  </label>
+                  <button type="submit">Send Message</button>
+                </form>
+              )}
+            </div>
+          </div>
+          <div className="pure-u-md-1-5"></div>
+          <div className="pure-u-1 pure-u-md-2-5">
+            <p>
+              Don't want to use the form? You can also email us directly at{" "}
+              <a href="mailto:hello@getoutline.com">hello@getoutline.com</a>{" "}
+              from your favorite email client.
+            </p>
+            <p>
+              We aim to reply to all enquiries within 24 hours, but we often
+              reply much faster than that during PST waking hours.
+            </p>
+          </div>
+        </div>
       </div>
-      <style jsx>
-        {`
-          .typeform-widget {
-            width: 100%;
-            height: 500px;
-            border: 1px solid ${colors.grey};
-            border-radius: 8px;
-          }
-        `}
-      </style>
+      <style jsx>{`
+        .form-wrapper {
+          padding: 12px 32px 32px;
+          border-radius: 8px;
+          background: ${colors.lightGrey};
+        }
+
+        textarea,
+        input,
+        select {
+          width: 100%;
+          padding: 8px;
+          border: 1px solid ${colors.greyDark};
+          border-radius: 4px;
+        }
+
+        textarea {
+          min-height: 180px;
+        }
+
+        label {
+          display: block;
+          margin: 12px 0;
+        }
+
+        label div {
+          font-weight: 500;
+        }
+
+        button {
+          display: inline-block;
+          background-color: ${colors.primary};
+          border-color: ${colors.primary};
+          color: ${colors.white};
+          border-width: 2px;
+          border-style: solid;
+          font-weight: 500;
+          cursor: pointer;
+          text-align: center;
+          border-radius: 6px;
+          padding: 10px 20px;
+          box-shadow: 0 2px 5px 0 rgba(3, 6, 26, 0.15);
+          font-size: 16px;
+          line-height: 1.8em;
+          white-space: nowrap;
+          height: 50px;
+        }
+
+        button:hover {
+          background-color: ${darken(0.1, colors.primary)};
+          border-color: ${darken(0.1, colors.primary)};
+          box-shadow: 0 1px 3px 0 rgba(3, 6, 26, 0.15);
+        }
+      `}</style>
     </Layout>
   );
 }
